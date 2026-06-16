0b3a7be7aa3cb0d59d9af9a0b18ee675aaea3504
Pam VanLeirsburg <pvanleirsburg@barrington220.org>
2026-06-16 11:01:41 -0500

    Display location information

---
+++ 'Za Hunter/LocationDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationDetailsView.swift
+//  'Za Hunter
+//
+//  Created by Pamela VanLeirsburg on 6/16/26.
+//
+
+import SwiftUI
+
+struct LocationDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationDetailsView()
+}
